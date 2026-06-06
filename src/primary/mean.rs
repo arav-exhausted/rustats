94ddccddb8be6596edaf21bfaaad58299d02df30
Om Barkare <ombarkare123@gmail.com>
2026-06-06 13:34:08 +0530

    primary/mean.rs remove generic from fn definition
    this makes it beginner friendly. Generics and traits will be
    bought up in later issues.

--- src/primary/mean.rs
+++ src/primary/mean.rs
@@ -10,10 +10,7 @@
 /// let avg = mean(values);
 /// assert_eq!(avg, Some(2.0));
 /// ```
-pub fn mean<I>(values: I) -> Option<f64>
-where
-    I: IntoIterator<Item = f64>,
-{
+pub fn mean(values: &[f64]) -> Option<f64> {
     let mut count: u64 = 0;
     let mut sum: f64 = 0.0;
 
