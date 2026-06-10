0d7b7c55af9cf7b4ceb3ddc61a7039d843822b87
arav-exhausted <aravprajapat1102@gmail.com>
2026-06-10 10:11:08 +0000

    Add range function with tests

--- src/primary/mod.rs
+++ src/primary/mod.rs
@@ -2,8 +2,12 @@ pub mod mean;
 pub mod median;
 pub mod mode;
 pub mod variance;
+pub mod range;
+
 
 pub use mean::mean;
 pub use median::median;
 pub use mode::mode;
 pub use variance::variance;
+
+pub use range::range;
