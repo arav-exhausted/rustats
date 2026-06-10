790f221ba3475b165a3e4f31e5c76d26d7b73a14
Om Barkare <ombarkare123@gmail.com>
2026-06-10 13:54:18 +0530

    remove use statemenet that was written twice

--- src/primary/mod.rs
+++ src/primary/mod.rs
@@ -1,12 +1,9 @@
 pub mod mean;
+pub mod median;
 pub mod mode;
 pub mod variance;
-pub mod median;
 
 pub use mean::mean;
+pub use median::median;
 pub use mode::mode;
 pub use variance::variance;
-
-pub use mean::mean;
-pub use mode::mode;
-pub use median::median;
